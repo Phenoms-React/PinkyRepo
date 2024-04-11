aa0c0657e84c65b9152951f436df5b3b6dd522e5
Agrawal <Pinky.Agrawal@in.pega.com>
2024-04-11 11:53:35 +0530

    Deleteitems

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -6,10 +6,22 @@ function ToDoItem({ item, todos, settodos }) {
 
     settodos(todos.filter((todo) => todo != item));
   }
+  function handleClick(item) {
+    console.log("item clicked" + item);
+    const newArray = todos.map((todo) =>
+      todo.name === item ? { ...todo, done: !todo.done } : { todo }
+    );
+    settodos(newArray);
+    console.log(todos);
+  }
+  const classNameDone = item.done ? styles.completed : "";
   return (
     <div className={styles.item}>
       <div classname={styles.itemname}>
-        {item.name}{" "}
+        <span className={classNameDone} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
+
         <span className={styles.deleteButton}>
           <button onClick={() => handleDelete(item)}>x</button>
         </span>
